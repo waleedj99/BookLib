0d4ea5e165e710e7c82d045841a36fd5034223c7
waleedj99 <waleedj1699@gmail.com>
2019-04-11 09:49:27 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
 var bodyParser = require('body-parser')
 const mongo = require('mongodb').MongoClient
 const url = 'mongodb://localhost:27017'
@@ -7,13 +8,16 @@ const url = 'mongodb://localhost:27017'
 app.use(bodyParser.urlencoded({ extended: false }))
 
 
-app.get('/register',function(req,res)
-{
-    res.sendFile(__dirname +'\\register.html');
+app.get('/register', function (req, res) {
+    var tagline = ''
+    res.render('pages/register.ejs', {
+        tagline: tagline
+    });
+    
 })
 
-app.get('/login',function(req,res){
-    res.sendFile(__dirname + '\\login.html');
+app.get('/login', function (req, res) {
+    res.render('pages/login.ejs');
 })
 
 
@@ -34,7 +38,7 @@ app.post('/login', function (req, res) {
         console.log("Connexted bois")
         var db = client.db('login')
         var collection = db.collection(req.body.username)
-        collection.findOne({ username: req.body.username },(err, item) => {
+        collection.findOne({ username: req.body.username }, (err, item) => {
             console.log("Founf" + item)
             if (item.password == req.body.password)
                 res.send("login Successful")
@@ -44,16 +48,17 @@ app.post('/login', function (req, res) {
         client.close();
     })
 })
-    
+
 
 //NEED TO FIND A WAY TO AVOID DUPLICATES
 
 app.post('/register', function (req, res) {
 
+var arr =[]
     console.log("THis sIS THE sTArt");
     console.log(req.body)
-    if(req.body.password == req.body.c_password){
-            
+    if (req.body.password == req.body.c_password) {
+
         mongo.connect(url, (err, client) => {
             if (err) {
                 console.error(err)
@@ -61,18 +66,51 @@ app.post('/register', function (req, res) {
             }
             console.log("Connexted bois")
             var db = client.db('login')
-            var collection = db.collection(req.body.username) 
+            var collection = db.collection(req.body.username)
+            
+
+
+            db.listCollections().toArray(function (err, collInfos) {
+                // collInfos is an array of collection info objects that look like:
+                collInfos.forEach((ele) => {
+                    //console.log(ele.name);
+                    arr.push(ele.name)
+                });
+
+                console.log(arr)
+                //collection.createIndex({ "username": 1 }, { unique: true });
+                n = arr.includes(req.body.username)
+                
+                if (n) {
+                    var tagline = "Username Taken"
+                    res.render('pages/register.ejs', {  
+                        tagline: tagline
+                    })
+                    console.log("Duplicate")
+                    
+                    //res.redirect('\dup')
+                } else {
+                    console.log("Adding")
+                    //var tagline = "Press Submit"
+                    //res.render('pages/login.ejs')
+                    console.log("Duplicate")
+                    //res.redirect('\login')
+                }
+            })
+
+
+
             collection.createIndex({ "username": 1 }, { unique: true });
-           
+
 
             console.log("Adding")
             collection.insertOne({ username: req.body.username, password: req.body.password }, (err, item) => {
-                res.redirect('\login')
+                res.render('pages/login.ejs')
             })
-           
+
             // collection.findOne({ username: req.body.username }, (err, item) => {
             //     console.log(item)
-                
+
             //     if (item){
             //         dup = true;
             //         res.redirect('\dup')
@@ -80,23 +118,22 @@ app.post('/register', function (req, res) {
             // })
 
             // console.log("Dup is " + dup)
-            
-           
 
-            
-                
+
+
+
+
             //res.redirect('\login');
             //...
             client.close()
-        })        
-    //     console.log(use.password)
+        })
+        //     console.log(use.password)
         //res.redirect('\login');
     }
-     else
-         res.send('Re_enter Password');
-    
+    else
+        res.send('Re_enter Password');
+
 })
 
 app.listen(3000);
-
 
