48f5d89dadb0b2297547f586ca45d79bbc2feac9
waleedj99 <waleedj1699@gmail.com>
2020-06-10 10:52:34 +0530

    added files for deployment

--- myapp/app.js
+++ myapp/app.js
@@ -450,4 +450,7 @@ app.post('/register', function (req, res) {
         })
     }
 })
-app.listen(3000);
+var port = process.env.PORT || 3000
+app.listen(port, function() {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
